525de4373a46e39a67811ec7aa47ba614782cc0c
avnms <avnmsarma@gmail.com>
2024-08-05 12:13:36 +1000

    Implement user register feature

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("register/", views.register_user, name="register"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
+import json
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 
-def home(request):
-    data = {"message": "welcome to Django!"}
-    return JsonResponse(data, safe=False)
+@csrf_exempt
+@require_POST
+def register_user(request):
+    data = json.loads(request.body)
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({"errors": "Username already exists"}, status=400)
+
+    user = User.objects.create(
+        username=username, email=email, password=make_password(password)
+    )
+
+    return JsonResponse({"username": user.username})
