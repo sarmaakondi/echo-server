4f3add9f79f468addeceafa1eac3ef6566a50dad
avnms <avnmsarma@gmail.com>
2024-08-07 08:13:48 +1000

    Update urls with create echo

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("register/", views.register_user, name="register_user"),
     path("login/", views.CustomTokenObtainPairSerializer.as_view(), name="login_user"),
     path("refresh/", views.refresh_token),
+    path("create-echo/", views.create_echo, name="create_echo"),
 ]
