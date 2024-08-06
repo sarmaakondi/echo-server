29aae3cf4c02d49db9500893551a846c277e61c6
avnms <avnmsarma@gmail.com>
2024-08-07 07:35:03 +1000

    Create echo model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Echo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="echoes")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Echo by {self.user.username}"
