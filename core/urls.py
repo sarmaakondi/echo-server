f10acb41c5da53bdc5336d231efe268cf0a3a3a3
avnms <avnmsarma@gmail.com>
2024-08-08 13:08:48 +1000

    Update urls

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("create-comment/", views.create_comment, name="create_comment"),
     path("like-echo/<int:echo_id>/", views.like_echo, name="like_echo"),
     path("list-echoes/", views.list_echoes, name="list_echoes"),
+    path("list-liked-echoes/", views.list_liked_echoes, name="list_liked_echoes"),
     path("list-echoes-no-auth/", views.list_echoes_no_auth, name="list_echoes_no_auth"),
 ]
